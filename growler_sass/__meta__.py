8788dfaa540b8d47d2abc4e87037e60a7470b3c1
Andrew Kubera <andrewkubera@gmail.com>
2016-10-01 13:09:02 -0400

    Fixed travis pytest call and renamed version to PEP-440 compatible "0.1.0dev0"

--- growler_sass/__meta__.py
+++ growler_sass/__meta__.py
@@ -5,7 +5,7 @@
 Metadata file for the growler-sass package.
 """
 
-version_info = (0, 1, 0, 'dev')
+version_info = (0, 1, 0, 'dev0')
 version = '.'.join(map(str, version_info))
 
 date = 'Sep 29, 2016'
